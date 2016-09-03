09b9a7dc04ba8d65c3bcc7bb98aeb731ed9ff960
Tyler Long <tyler4long@gmail.com>
2016-09-03 11:03:29 +0800

    Readyn for Bluemix

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "glip-hubot-xia" %* 
+node_modules\.bin\hubot.cmd --name "xia" %*
